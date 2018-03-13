58ee6e9f7cdf908541549a4f88ff9e782867e042
THEtheChad <chad.d.elliott@gmail.com>
2018-03-13 13:16:28 -0500

    modified exports

--- src/index.js
+++ src/index.js
@@ -2,4 +2,7 @@ import Client from './client'
 import Server from './server'
 import worker from './worker'
 
-export { Client, Server, worker }
+export Client
+export Worker
+export Server
+export default { Client, Server, worker }
