a9ec695b004fd8932d6e0e827c39cfb067ea29df
THEtheChad <chad.d.elliott@gmail.com>
2018-04-19 15:07:11 -0500

    added daemon using pm2

---
+++ src/server/pm2.js
@@ -0,0 +1,17 @@
+import pm2 from 'pm2'
+
+console.log('mounting to pm2 process...')
+pm2.connect(err => {
+  if (err) {
+    console.error(err)
+    process.exit(2)
+  }
+
+  pm2.start({
+    name: 'jobqueue',
+    script: './lib/server/index.js',
+  }, (err, apps) => {
+    pm2.disconnect()
+    if (err) throw err
+  })
+})
